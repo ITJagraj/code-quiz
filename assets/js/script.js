be251ff996075ef0e0a00af6493181bdd4e1f557
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-21 19:48:00 -0400

    added more questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-var skip = document.getElementById
+var skip = document.getElementById('skip');
+var score = document.getElementById('socre');
+var totalscore = document.getElementById('totalsocre');
+var countdown = document.getElementById('coundown');
+var c
