d6604b68e9544240cdc4af7ac78c53f878af9682
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-21 22:50:23 -0400

    listarray

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,57 +39,33 @@ var questionBlock = `<h4>${questions[currentQuestion].question}</h4>
 
 }
 var questions = [{
-    question: "2. In the DOM’s event object, what does its target property refer to?", 
-    option1: "Hyperlinks and text Markup Language",
-    option2: "Home tool Markup Language",
-    option3: "Hyper Text Markup Language",
-    option4: "Hyper Text Manipulating Language",
-}]
-function code() {
-    var questionBlock = `<h4>${questions[currentQuestion].question}</h4>
-    <div class="qa_ans_row">
-        <input type="radio" name="a1">
-        <span>${questions[currentQuestion].option1}</span>
-    </div>
-        <div class="qa_ans_row">
-            <input type="radio" name="a1">
-            <span>${questions[currentQuestion].option2}</span>
-        </div>
-            <div class="qa_ans_row">
-                <input type="radio" name="a1" valid= "valid">
-                <span>${questions[currentQuestion].option3}</span>
-            </div>
-                <div class="qa_ans_row">
-                    <input type="radio" name="a1">
-                    <span>${questions[currentQuestion].option4}</span>
-                </div>`
-                qaBody.innerHTML = questionBlock
+    question: "2. In the DOM's event object, what does its target property refer to?", 
+    option1: "It refers to the HTML element that was interacted with to dispatch the event",
+    option2: "It refers to the HTML element we want to affect as a result of our dispatched event.",
+{
+    question: "question",
+    option1: "1",
+    option2: "2",
+}
+]
+var questions = [{
+    question: "2. What does event.preventDefault() do?", 
+    option1: "It stops the browser from reloading the page upon a form submission.",
+    option2: "It stops the browser from allowing the form submission event to occur.",
+{
+    question: "question",
+    option1: "1",
+    option2: "2",
+}
+]
 
-function option() {
-var questionBlock = `<h4>${questions[currentQuestion].question}</h4>
-<div class="qa_ans_row">
-    <input type="radio" name="a1">
-    <span>${questions[currentQuestion].option1}</span>
-</div>
-    <div class="qa_ans_row">
-        <input type="radio" name="a1">
-        <span>${questions[currentQuestion].option2}</span>
-    </div>
-        <div class="qa_ans_row">
-            <input type="radio" name="a1" valid= "valid">
-            <span>${questions[currentQuestion].option3}</span>
-        </div>
-            <div class="qa_ans_row">
-                <input type="radio" name="a1">
-                <span>${questions[currentQuestion].option4}</span>
-            </div>`
-            qaBody.innerHTML = questionBlock
 
 skip.addEventListener('click', function() {
     step();
 })
 function step() {
-    count += 1;
+    currentQuestion += 1;
+    code();
     for (var i = 0; 1 < qaSet.length; i++ ) {
         qaSet[i].className = 'qa_set' ;
     }
