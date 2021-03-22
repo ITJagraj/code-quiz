074c4b0c4225a1b4b1502a7e35e80866a705cd16
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-22 11:35:55 -0400

    scorecount

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,23 @@ var questions = [{
     option2: "Home tool Markup Language",
     option3: "Hyper Text Markup Language",
     option4: "Hyper Text Manipulating Language",
-}]
+}, 
+{
+    question: "3. What does event.preventDefault() do?", 
+    option1: "Not defined",
+    option2: "It stops the browser from allowing the form submission event to occur.",
+    option3: "It stops the browser from reloading the page upon a form submission.",
+    option4: "Does not do anything",
+},
+{
+    question: "2. In the DOM's event object, what does its target property refer to?", 
+    option1: "It refers to the HTML element that was interacted with to dispatch the event.",
+    option2: "It refers to the HTML element we want to affect as a result of our dispatched event.",
+    option3: "Not defined",
+    option4: "Wrong statement",
+}
+]
+
 
 function code() {
 var questionBlock = `<h4>${questions[currentQuestion].question}</h4>
@@ -40,34 +56,28 @@ var questionBlock = `<h4>${questions[currentQuestion].question}</h4>
 
 }
 
-var scores = [{
-    question: "2. In the DOM’s event object, what does its target property refer to?", 
-    option1: "It refers to the HTML element that was interacted with to dispatch the event.",
-    option2: "It refers to the HTML element we want to affect as a result of our dispatched event.",
-    option3: "Not defined",
-    option4: "Wrong statement",
-}]
-
-var names = [{
-    question: "3. What does event.preventDefault() do?", 
-    option1: "Not defined",
-    option2: "It stops the browser from allowing the form submission event to occur.",
-    option3: "It stops the browser from reloading the page upon a form submission.",
-    option4: "Does not do anything",
-}]
-
 skip.addEventListener('click', function() {
     step();
 })
+qaAnsRow.forEach(function(qaAnsRowSingle){
+    qaAnsRowSingle.addEventListener('click',function(){
+        setTimeout(function(){
+            step();
+        },500)
+        var valid = this.getattribute("valid");
+        if(valid == "valid"){
+            scoreCount +=20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+        }else {
+            scoreCount -=20;
+            score.innerHTML = scoreCount;
+            totalScore.innerHTML = scoreCount;
+
+        }
+});
 function step() {
     console.log(qaSet);
+    currentQuestion += 1;
     code();
-    for (var i = 0; i < qaSet.length; i++ ) {
-        qaSet[i].className = 'qa_set' ;
-    }
-    qaSet[count].className = 'qa_set active'; 
-    if(count == 5) {
-        skip.style.display = none;
-    }
-}
-code();
+};
