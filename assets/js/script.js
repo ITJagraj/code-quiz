4531c5e9fc4d6ce49c57ac0c38f45efd5f942575
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-21 20:40:10 -0400

    creatingvariables

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,8 @@ var skip = document.getElementById('skip');
 var score = document.getElementById('socre');
 var totalscore = document.getElementById('totalsocre');
 var countdown = document.getElementById('coundown');
-var c
+var count = 0;
+var scorecount = 0;
+var duration = 0;
+var qaSet = document.querySelectorAll('.qa_set');
+var qaAnswer = document.querySelectorAll('.qa_set .qa_ans_row input');
