6ba1bbb9c23fc0f4cf5bf505a0c11cea21c0f705
Jagraj Singh <singh.jagraj96@yahoo.com>
2021-03-25 15:29:01 -0400

    changingquestions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,46 @@
 var skip = document.getElementById('skip');
-var score = document.getElementById('socre');
+var score = document.getElementById('score');
 var totalScore = document.getElementById('totalsocre');
 var countDown = document.getElementById('countdown');
-var count = 0;
-var scoreCount = 0;
-var duration = 0;
+var score = 0;
 var counter = 0;
 var currentQuestion = 0;
-var question = 0;
 var qaSet = document.querySelectorAll('.qa_set');
 var qaAnsRow = document.querySelectorAll('.qa_set .qa_ans_row input');
 var qaBody = document.querySelector('.qa_body');
 var questions = [{
     question: "1. What does HTML stands for ?",
-    option1: "Hyperlinks and text Markup Language",
-    option2: "Home tool Markup Language",
-    option3: "Hyper Text Markup Language",
-    option4: "Hyper Text Manipulating Language",
-    correctAnswer: 'option3',
+    options: ["Hyperlinks and text Markup Language", "Home tool Markup Language", "Hyper Text Markup Language", "Hyper Text Manipulating Language"],
+    correctAnswer: "Hyper Text Markup Language"
 },
 {
     question: "2. What does event.preventDefault() do?",
-    option1: "Not defined",
-    option2: "It stops the browser from allowing the form submission event to occur.",
-    option3: "It stops the browser from reloading the page upon a form submission.",
-    option4: "Does not do anything",
+    options: ["Not defined", "It stops the browser from allowing the form submission event to occur.", "It stops the browser from reloading the page upon a form submission.", "Does not do anything"],
+     correctAnswer: "It stops the browser from reloading the page upon a form submission.",
 },
-{
-    question: "3. In the DOM's event object, what does its target property refer to?",
-    option1: "Not defined",
-    option2: "It refers to the HTML element we want to affect as a result of our dispatched event.",
-    option3: "It refers to the HTML element that was interacted with to dispatch the event.",
-    option4: "Wrong statement",
-},
-{
-    question: "4. Which symbol is used for comments in Javascript?",
-    option1: "// for Single line comments",
-    option2: "/* Multi",
-    option3: "Line",
-    option4: "*/",  
-}
+// {
+//     question: "3. In the DOM's event object, what does its target property refer to?",
+//     option1: "Not defined",
+//     option2: "It refers to the HTML element we want to affect as a result of our dispatched event.",
+//     option3: "It refers to the HTML element that was interacted with to dispatch the event.",
+//     option4: "Wrong statement",
+//     correctAnswer: "option3",
+// },
+// {
+//     question: "4. Which symbol is used for comments in Javascript?",
+//     option1: "// for Single line comments",
+//     option2: "/* Multi",
+//     option3: "Line",
+//     option4: "*/",  
+//     correctAnswer: "option3",
+// }
 ]
-
+correctAnswer.addEventListener('click', function (){
+    next();
+})
 function next() {
-    var user_answer = document.querySelector("questions.option.active").innerHTML
-    if (user_answer === questions[currentQuestion].correctAnswer){
-        point +=10;
-    }
-    question_count++;
-    console.log(scoreCount);
+ var count = localStorage.getItem('correctAnswer');
+
 }
 
 
